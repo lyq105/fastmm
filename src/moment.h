f58fe0e1eddd055b2d3d5194a31e4351b7a00d8d
liyiqiang <liyiqiang@mail.nwpu.edu.cn>
2012-02-22 09:29:03 +0800

    add a template for calculate moments

---
+++ src/moment.h
@@ -0,0 +1,22 @@
+// \file The head of moment calculating.
+//
+//
+
+#ifndef _MOMENT_H_
+#define _MOMENT_H_
+
+#include "quadtree.h"
+
+// 计算多极矩
+void cal_multipole_moment(Quadtree& qtree,int cellindex,Mesh mesh,double* data); 
+// 传递多极矩到父节点
+void transfer_mm_to_its_father_by_m2m(Quadtree& qtree,int cellindex);
+// 将交互节点的多极矩转化为中心节点的局部矩
+void transfer_lm_by_m2l(Quadtree& qtree,int cellIndex);	
+// 将父节点的局部矩转换为当前节点的局部矩
+void transfer_lm_by_l2l(Quadtree& qtree, int cellIndex);
+
+
+
+
+#endif // _MOMENT_H_
